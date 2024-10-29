57b4a0bddd3d099f90eca1d99477358933ef3713
irAbs174 <damerchiloa@gmail.com>
2024-10-30 02:44:02 +0330

    > add verbus_name to customer and image field ScreenShot model index/tracker_models.py file

--- app/core/index/tracker_models.py
+++ app/core/index/tracker_models.py
@@ -4,10 +4,10 @@ class ScreenShot(models.Model):
     orders_number = models.CharField(unique=True, max_length=16, verbose_name='شماره سفارش', null=True, blank=True)
     tracking_number = models.CharField(max_length=64, verbose_name='کد رهگیری', null=True, blank=True)
     phone_number = models.CharField(max_length=12, verbose_name='شماره تماس', null=True, blank=True)
-    customer = models.CharField(max_length=65, verbose_name='نام کامل مشتری')
-    image = models.ImageField(upload_to='screen_shots/', verbose_name='اسکرین شات')
+    customer = models.CharField(max_length=65, verbose_name='نام کامل مشتری', blank=True)
+    image = models.ImageField(upload_to='screen_shots/', verbose_name='اسکرین شات', blank=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ بارگذاری اسکرین شات')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='آخرین به روزرسانی')
     
     def __str__(self):
-        return f"=> <{self.orders_number}> ــ_< {self.customer} > <="
+        return f"=> {self.tracking_number} <="
