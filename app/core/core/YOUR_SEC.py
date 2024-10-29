a1c49b3d5a69d2d0f15b3776383862e2afea17bc
irAbs174 <damerchiloa@gmail.com>
2024-10-30 02:40:36 +0330

    -> Define variable for post portal url in SEC.py and YOUR_SEC.py

--- app/core/core/YOUR_SEC.py
+++ app/core/core/YOUR_SEC.py
@@ -1,4 +1,8 @@
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'PASTE_YOUR_DJANGO_SECRET_KEY_HERE'
 
+# Tracking post portal url configure
+tracking_url = "https://tracking.post.ir"
+
+# Media forlder path configure
 media_dir = "/home/public/media/post_tracker"
