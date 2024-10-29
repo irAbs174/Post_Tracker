efa2ed4d61700af1faa132b19938147cac31f118
irAbs174 <damerchiloa@gmail.com>
2024-10-30 02:41:52 +0330

    added new chnage : > Send tracking variables as arg => tools dir
    > Pepare required PNG image screen shot
    
    > Define variable form_data for sort returned data
    
    > Initialize the form with the data
    
    > Store returned data with django form

--- app/core/index/api.py
+++ app/core/index/api.py
@@ -1,21 +1,49 @@
+from tools.driver_functions import DriverFunctions as DF
 from django.views.decorators.csrf import csrf_exempt
+from django.core.files.base import ContentFile
+from core.SEC import tracking_url as portal
+from .tracker_forms import ScreenShotForm
+from .tracker_models import ScreenShot
 from django.http import JsonResponse
-from tools import (
-    driver_functions,
-    tracking_post
-)
-
 
 @csrf_exempt
-def test(request):
+def store_tracking_screenshot(request):
     success = False
     # Test Request => curl -d "tracking_code=SIMPLE_CODE" -X POST HOST:PORT
     if request.method == "POST":
         try:
+            # Send tracking variables as arg => tools dir
             tracking_code = request.POST.get('tracking_code')
-            result = tracking_post.Tracker(tracking_code).run()
+            # Prepare required PNG image screen shot
+            res = DF(portal, tracking_code).run()
+            screen_shot = ContentFile(res['img'], name=f"{tracking_code}.png")
+            
+            # Define variable form_data for sort returned data
+            form_data = {
+                'orders_number': None,
+                'tracking_number': tracking_code,
+                'phone_number': None,
+                'customer': None,
+            }
+            
+            # Initialize the form with the data
+            form = ScreenShotForm(data=form_data)
+            
+            # Store returned data with django form
+            screenshot_instance = form.save(commit=False)
+            screenshot_instance.image.save(f"{tracking_code}.png", screen_shot)
+            screenshot_instance.save()
+            
+            """ Prepare json response dara include: 
+                status as (int), result as(dict) & success as(bol)
+                variables for return """
+            
             status = 200
             success = True
+            result = {
+                'tracking_code': screenshot_instance.tracking_number,
+                'img': screenshot_instance.image.url
+            }
             
         except Exception as error:
             print(f"ERROR +>>>\n{error}")
