45013c6993911aa182d68609f1665ccb3d3fa72c
irAbs174 <damerchiloa@gmail.com>
2024-10-30 12:10:04 +0330

    Create bash script file for simple request ro server

--- app/core/tools/simple_curl_request.sh
+++ app/core/tools/simple_curl_request.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-code="195372322400003260665111"
 response_file="server_response.json"
 
-curl -s -d "tracking_code=$code" -X POST "http://127.0.0.1:8000/take_shot" >> "$response_file"
+curl -s -d "order_code=3043897" -X POST "http://127.0.0.1:8000/report" >> "$response_file"
 echo "" >> "$response_file"
-grep -i "$code" "$response_file"
+grep -i "3043897" "$response_file"
