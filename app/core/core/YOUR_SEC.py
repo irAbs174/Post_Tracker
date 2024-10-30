d9c5f08663f0212105d121a181b32e391e27eda5
irAbs174 <damerchiloa@gmail.com>
2024-10-30 06:13:10 +0330

    move debug cariable to SEC.py(YOUR_SEC.py)

--- app/core/core/YOUR_SEC.py
+++ app/core/core/YOUR_SEC.py
@@ -1,8 +1,14 @@
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'PASTE_YOUR_DJANGO_SECRET_KEY_HERE'
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# Production status (options: 'live' and 'dev')
+production_status = 'dev' if DEBUG == True else 'live'
+
 # Tracking post portal url configure
-tracking_url = "https://tracking.post.ir"
+tracking_url = "https://post.gov"
 
 # Media forlder path configure
 media_dir = "/home/public/media/post_tracker"
--- app/core/core/settings.py
+++ app/core/core/settings.py
@@ -5,9 +5,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 # Application definition
